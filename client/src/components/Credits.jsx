0428ce17dc596850431a8ed34be86328e7ce2a8b
Zakaria TOUIL <96567757+toouil@users.noreply.github.com>
2025-05-25 15:58:40 +0100

    Update: Github profile

--- client/src/components/Credits.jsx
+++ client/src/components/Credits.jsx
@@ -3,7 +3,7 @@ import { Github } from "../assets/icons";
 export default function Credits() {
   return (
     <section className="credits">
-      <a href="https://github.com/zakaria-tl" id="credits">
+      <a href="https://github.com/toouil" id="credits">
         <Github /> Created by KYROS
       </a>
     </section>
